4c3dc16e4d3e4356668084b254356f2367aa6cf2
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-04-16 08:34:31 +0530

    responsiveness

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -50,20 +50,20 @@ function AddTodo({ where }) {
         <form onSubmit={addTodoHandler} className='w-full h-fit bg-[#365486] flex justify-center gap-x-4 md:gap-x-16 xl:gap-x-20 py-1 mx-auto rounded-lg  '>
             <input
                 type="text"
-                className='text-[#0F1035] bg-[#DCF2F1] rounded-lg text-center border-transparent p-0 lg:h-10 h-8 w-36 lg:w-fit text-sm lg:text-lg'
+                className='text-[#0F1035] bg-[#DCF2F1] rounded-lg text-center border-transparent p-0 lg:h-10 h-8 md:w-36 w-28 lg:w-fit text-md lg:text-lg'
                 value={text}
                 onChange={(e) => setText(e.target.value)}
                 placeholder='Enter Task'
                 />
             <input
                 type="date"
-                className='text-[#0F1035] bg-[#DCF2F1] cursor-pointer border-transparent text-center rounded-lg lg:h-10 h-8  text-sm lg:text-lg'
+                className='text-[#0F1035] bg-[#DCF2F1] cursor-pointer border-transparent text-center rounded-lg lg:h-10 h-8 placeholder:text-[#0F1035] text-md lg:text-lg'
                 value={date}
                 onChange={(e) => setDate(e.target.value)}
                 />
             <button
                 type='submit'
-                className=' text-[#0F1035] bg-[#DCF2F1] p-0 rounded-lg lg:p-2  lg:h-10 h-8  text-[10px] lg:text-lg '
+                className=' text-[#0F1035] bg-[#DCF2F1] -pl-10 md:-pl-6 rounded-lg lg:p-2  lg:h-10 h-8  text-md lg:text-lg '
                 >Create Task</button>
 
         </form>
